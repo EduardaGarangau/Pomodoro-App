f38fe787487d733c81f98d8ce2dd316e7eea9c6a
Eduarda Garangau <eduardagarangau98@gmail.com>
2022-10-19 17:23:31 -0300

    Timer Dialog Created

--- lib/widgets/home/break_dialog.dart
+++ lib/widgets/home/break_dialog.dart
@@ -9,35 +9,86 @@ class BreakDialog extends StatelessWidget {
       future: Future.delayed(const Duration(seconds: 0), () {
         showDialog(
           context: context,
-          builder: (context) => AlertDialog(
-            backgroundColor: Colors.white,
-            title: const Center(
-              child: Text(
-                'BREAK OVER!!',
-                style: TextStyle(
-                  fontFamily: 'Nunito',
-                  color: Colors.green,
-                  fontWeight: FontWeight.bold,
+          builder: (context) => Dialog(
+            backgroundColor: Colors.transparent,
+            insetPadding: const EdgeInsets.all(10),
+            child: Stack(
+              clipBehavior: Clip.none,
+              alignment: Alignment.center,
+              children: <Widget>[
+                Container(
+                  width: 400,
+                  height: 280,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    color: Colors.green,
+                  ),
+                  padding: const EdgeInsets.fromLTRB(20, 50, 20, 20),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const SizedBox(height: 15),
+                      const Text(
+                        "BREAK TIME OVER!!!",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 30,
+                          fontFamily: 'Nunito',
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          shadows: [
+                            Shadow(
+                              color: Colors.black,
+                              offset: Offset(3, 3),
+                            ),
+                          ],
+                        ),
+                      ),
+                      const Text(
+                        "Time to get back to work, you can do this 😎",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontFamily: 'Nunito',
+                          color: Colors.white,
+                        ),
+                      ),
+                      const SizedBox(height: 35),
+                      SizedBox(
+                        width: 200,
+                        height: 50,
+                        child: ElevatedButton(
+                          child: const Text(
+                            "CLOSE",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontFamily: 'Nunito',
+                              color: Colors.white,
+                            ),
+                          ),
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.black,
+                            shape: const StadiumBorder(),
+                          ),
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
+                Positioned(
+                  top: -65,
+                  child: Image.asset(
+                    'lib/assets/images/alarm.gif',
+                    width: 150,
+                    height: 150,
+                  ),
+                )
+              ],
             ),
-            actions: [
-              Center(
-                child: ElevatedButton(
-                  child: Icon(
-                    Icons.timer_off_sharp,
-                    color: Colors.white,
-                  ),
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.green,
-                    shape: const StadiumBorder(),
-                  ),
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                  },
-                ),
-              ),
-            ],
           ),
         );
       }),
--- lib/widgets/home/work_dialog.dart
+++ lib/widgets/home/work_dialog.dart
@@ -1,3 +1,4 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
 class WorkDialog extends StatelessWidget {
@@ -5,37 +6,98 @@ class WorkDialog extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final player = AudioPlayer();
+
     return FutureBuilder(
-      future: Future.delayed(const Duration(seconds: 0), () {
+      future: Future.delayed(const Duration(seconds: 0), () async {
+        final cache = AudioCache(prefix: '/assets');
+        final url = await cache.load('sound_alarm.wav');
+        await player.play(UrlSource(url.path));
+        print(player.state);
+
         showDialog(
           context: context,
-          builder: (context) => AlertDialog(
-            title: Center(
-              child: Text(
-                'WORK OVER!!',
-                style: TextStyle(
-                  fontFamily: 'Nunito',
-                  color: Colors.red.shade700,
+          builder: (context) => Dialog(
+            backgroundColor: Colors.transparent,
+            insetPadding: const EdgeInsets.all(10),
+            child: Stack(
+              clipBehavior: Clip.none,
+              alignment: Alignment.center,
+              children: <Widget>[
+                Container(
+                  width: 400,
+                  height: 280,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    color: const Color.fromRGBO(192, 34, 67, 1),
+                  ),
+                  padding: const EdgeInsets.fromLTRB(20, 50, 20, 20),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const SizedBox(height: 15),
+                      const Text(
+                        "WORK TIME OVER!!!",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 30,
+                          fontFamily: 'Nunito',
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          shadows: [
+                            Shadow(
+                              color: Colors.black,
+                              offset: Offset(3, 3),
+                            ),
+                          ],
+                        ),
+                      ),
+                      const Text(
+                        "Congratulations, you made it! You deserve a break 🥳",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontFamily: 'Nunito',
+                          color: Colors.white,
+                        ),
+                      ),
+                      const SizedBox(height: 35),
+                      SizedBox(
+                        width: 200,
+                        height: 50,
+                        child: ElevatedButton(
+                          child: const Text(
+                            "CLOSE",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontFamily: 'Nunito',
+                              color: Colors.white,
+                            ),
+                          ),
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.black,
+                            shape: const StadiumBorder(),
+                          ),
+                          onPressed: () async {
+                            Navigator.of(context).pop();
+                            await player.stop();
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
+                Positioned(
+                  top: -65,
+                  child: Image.asset(
+                    'lib/assets/images/alarm.gif',
+                    width: 150,
+                    height: 150,
+                  ),
+                )
+              ],
             ),
-            actions: [
-              Center(
-                child: ElevatedButton(
-                  child: Icon(
-                    Icons.timer_off_sharp,
-                    color: Colors.white,
-                  ),
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.red.shade700,
-                    shape: const StadiumBorder(),
-                  ),
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                  },
-                ),
-              ),
-            ],
           ),
         );
       }),
